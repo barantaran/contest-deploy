fc1bfcc284ec5e5c91d75de326ca362f27694c29
barantaran <yourchev@gmail.com>
2016-03-16 12:38:00 +0300

    Init app with one file only

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-composer selfupdate
+echo 'Installing app'
 composer install
+echo 'Trying to start deploy manually from www-data user'
+sudo -H -u www-data vendor/deployer/deployer/bin/dep deploy dev
