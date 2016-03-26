06f67103e879992670e3ab50e4f4206f3eb105f8
barantaran <yourchev@gmail.com>
2016-03-26 20:44:18 +0300

    Move logs to local file

--- deploy-loop.php
+++ deploy-loop.php
@@ -12,22 +12,23 @@
  */
 
 require "vendor/autoload.php";
+use GitHubWebhook\Handler,
+    Monolog\Handler\StreamHandler,
+    Monolog\Logger;
 
-use GitHubWebhook\Handler;
-
-$githubWebhookSecret = "";
-$logentriesToken = "";
+$githubWebhookSecret = "GjkmLbhfr2016KM";
 
 $handler = new Handler($githubWebhookSecret, __DIR__);
+$logHandler = new StreamHandler('deploy.log', Logger::INFO);
 
-$handler->startLoggerInfo($logentriesToken);
+$handler->startLogger($logHandler);
 
 $handler->masterMerge(
     function ($data) {
         putenv("HOME=/home/barantaran");
-        shell_exec("/home/www/contest/vendor/deployer/deployer/bin/dep deploy dev > deploy.log 2>deploy.log &");
+        shell_exec("/home/www/contest/vendor/deployer/deployer/bin/dep deploy dev >> deploy.log 2>>deploy.log &");
         echo "deploying";
     }
 );
 
-$handler->handle();
+echo $handler->handle();
