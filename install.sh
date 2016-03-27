73be08b98ace6cc35e606722c39349dcb14983f0
barantaran <yourchev@gmail.com>
2016-03-27 13:07:21 +0300

    Improve tip

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 echo 'Installing app'
 composer install
 echo ""
-echo "$(tput setaf 0)$(tput setab 2)Confifure servers.yml and phinx.yml properly then run:$(tput sgr 0)"
+echo "$(tput setaf 0)$(tput setab 2)Ensure that document folder has correct premissions set."
+echo "Confifure servers.yml and phinx.yml properly then run:$(tput sgr 0)"
 echo "sudo -H -u www-data vendor/deployer/deployer/bin/dep deploy dev"
 echo ""
